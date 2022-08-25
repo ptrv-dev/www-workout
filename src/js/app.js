7cc58654f734cfc827a808039c0888cb93f229e8
ptrv-dev <petrov99119@gmail.com>
2022-08-25 14:17:14 +0300

    Section Latest Training is done.

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,22 @@ burgerButton.addEventListener("click", (e) => {
     headerNav.classList.toggle("nav_active");
     document.body.classList.toggle("no-scroll");
 })
+
+// Dynamic adaptive
+
+const dynamicAdaptive = () => {
+    const link = document.querySelector(".latest-training__link");
+    if (window.innerWidth <= 425) {
+        document.querySelector(".latest-training__content").append(link);
+        link.classList.add("button");
+        link.innerHTML = "See more";
+    } else {
+        document.querySelector(".latest-training__header").append(link);
+        if (link.classList.contains("button")) link.classList.remove("button");
+        if (link.innerHTML === "See more") link.innerHTML = "See more here";
+    }
+}
+
+
+window.addEventListener("resize", () => dynamicAdaptive());
+window.addEventListener("load", () => dynamicAdaptive());
